8b6c0b1b39b0bfb5ca2fdb44b253d691f04c9f99
Kaven Kim <kavenkim@Kavens-MacBook-Pro.local>
2022-06-30 10:52:35 -0400

    Configured client-side routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +12,14 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Page Count: {count}</h1>
+      <Routes>
+        <Route path="testing">
+          <h1>Test Route</h1>
+        </Route>
+        <Route path="/">
+          <h1>Page Count: {count}</h1>
+        </Route>
+      </Routes>
     </div>
   );
 }
